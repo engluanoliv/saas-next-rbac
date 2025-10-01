b5b164db3cf972cd1220660e37cdf03e74f7c4c8
engluanoliv <engluanoliv@gmail.com>
2025-09-30 21:25:37 -0300

    feat: created the dependencies auth

--- config/eslint-config/node.js
+++ config/eslint-config/node.js
@@ -4,7 +4,7 @@
  * @type {import("eslint").Linter.Config[]}
  * */
 module.exports = {
-  extends: ['@rocketseat/eslint-config/next' ],
+  extends: ['@rocketseat/eslint-config/node'],
   plugins: ['simple-import-sort'],
-  rules: {'simple-import-sort/imports': 'error'},
+  rules: { 'simple-import-sort/imports': 'error' },
 }
